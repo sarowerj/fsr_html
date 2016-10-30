3de564c1ff32a2833d872393fc36c127fa9cd7c1
Sarower Jahan <sarowerj@gmail.com>
2016-10-31 02:14:31 +0600

    Basic plugin installed

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Title for website -->
+    <title>FSR | Flight Safety & Regulation</title>
+
+    <!-- Bootstrap CSS v3.3.7 -->
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- jQuery v3.1.1 -->
+    <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script> 
+    
+    <!-- Including Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+    
+    <!-- Including Main Theme CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <header>
+          <div class="container">
+              <div class="row">
+                  <div class="col-xs-6 local">
+                      <span class="bold">Sarower SAROWER</span><br />
+                      <span class="reg">Sarower SAROWER</span><br />
+                      <span class="light">Sarower SAROWER</span><br />
+                  </div>
+                  <div class="col-xs-6 col-lg-offset-1dfd google">
+                      <span class="bold">Sarower SAROWER</span><br />
+                      <span class="reg">Sarower SAROWER</span><br />
+                      <span class="light">Sarower SAROWER</span><br />
+                  </div>
+              </div>
+          </div>
+      </header>
+
+    
+    <!-- Bootstrap JS v3.3.7-->
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
