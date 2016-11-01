0eacddab2c8b7ec57e5e8bc24e8559cfc69d463f
Sarower Jahan <sarowerj@gmail.com>
2016-11-02 01:15:15 +0600

    Header Created

--- index.php
+++ index.php
@@ -1,49 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Title for website -->
-    <title>FSR | Flight Safety &amp; Regulation</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Title for website -->
+        <title>FSR | Flight Safety &amp; Regulation</title>
 
-    <!-- Bootstrap CSS v3.3.7 -->
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- jQuery v3.1.1 -->
-    <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script> 
-    
-    <!-- Including Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
-    
-    <!-- Including Main Theme CSS -->
-    <link rel="stylesheet" href="css/style.css" />
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-      <header>
-          <div class="container">
-              <div class="row">
-                  <div class="col-xs-6 local">
-                      <span class="bold">Sarower SAROWER</span><br />
-                      <span class="reg">Sarower SAROWER</span><br />
-                      <span class="light">Sarower SAROWER</span><br />
-                  </div>
-                  <div class="col-xs-6 col-lg-offset-1dfd google">
-                      <span class="bold">Sarower SAROWER</span><br />
-                      <span class="reg">Sarower SAROWER</span><br />
-                      <span class="light">Sarower SAROWER</span><br />
-                  </div>
-              </div>
-          </div>
-      </header>
+        <!-- Bootstrap CSS v3.3.7 -->
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    
-    <!-- Bootstrap JS v3.3.7-->
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  </body>
+        <!-- jQuery v3.1.1 -->
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script> 
+
+        <!-- Including Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+        <link href="css/fonts.css" rel="stylesheet">
+        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css" />
+
+        <!-- Including Main Theme CSS -->
+        <link rel="stylesheet" href="css/style.css" />
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7">
+                        <div class="logo">
+                            <a href="index.php"><img src="images/fsr-logo.png" alt="" /></a>
+                        </div>
+                        <div class="office_name">
+                            <div class="english">
+                                <h1>Flight Safety & Regulation</h1>
+                            </div>
+                            <div class="bangla">
+                                <h2>বাংলাদেশ বেসামরিক বিমান চলাচল কতৃপক্ষ </h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-4 col-lg-offset-1">
+                        <ul class="top_nav_bar">
+                            <li><a href="#">About CAAB</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Log In</a></li>
+                        </ul>
+                        <div class="search_form">
+                            <form action="#">
+                                <input type="text" name="search_fld" placeholder="Type to search" />
+                                <input type="submit" value="Search" />
+                                <i class="fa fa-caret-down" aria-hidden="true"></i>
+                                <i class="fa fa-search"></i>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </header>
+
+
+        <!-- Bootstrap JS v3.3.7-->
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
 </html>
